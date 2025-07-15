f64207f568106f2a17ebe2e7512654d5be9dc612
nileshdhepale <nildhepale997@gmail.com>
2025-07-15 18:42:13 +0530

    added url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   // baseUrl: 'https://library-management-backend-l8eu4f82t.vercel.app/api'
-  baseUrl: 'https://library-management-backend-otomh3j00.vercel.app/api',
+  baseUrl:
+    'https://library-management-git-11cdaf-nildhepale997-gmailcoms-projects.vercel.app/api',
 };
